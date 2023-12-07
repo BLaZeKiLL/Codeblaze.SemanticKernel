7bd0086ced42db89655d5166a80770dcb0854b13
Devashish Lal <laldevashish@gmail.com>
2023-12-06 20:05:10 -0800

    embedding warn fix

--- Codeblaze.SemanticKernel.Connectors.AI.Ollama/EmbeddingGeneration/OllamaTextEmbeddingGeneration.cs
+++ Codeblaze.SemanticKernel.Connectors.AI.Ollama/EmbeddingGeneration/OllamaTextEmbeddingGeneration.cs
@@ -7,9 +7,11 @@ using Microsoft.SemanticKernel.AI.Embeddings;
 
 namespace Codeblaze.SemanticKernel.Connectors.AI.Ollama;
 
+#pragma warning disable SKEXP0011
 public class OllamaTextEmbeddingGeneration(string modelId, string baseUrl, HttpClient http, ILoggerFactory? loggerFactory)
     : OllamaBase<OllamaTextEmbeddingGeneration>(modelId, baseUrl, http, loggerFactory),
         ITextEmbeddingGeneration
+#pragma warning restore SKEXP0011
 {
     public async Task<IList<ReadOnlyMemory<float>>> GenerateEmbeddingsAsync(IList<string> data, Kernel? kernel = null,
         CancellationToken cancellationToken = new())
